f87dd3264c87f8b74d9c48841651e4656375ec36
Dev-El-badry <miliarder96@gmail.com>
2018-05-17 17:20:35 +0200

    changes clean up blade files and manage backend

---
+++ resources/views/_includes/nav/main.blade.php
@@ -0,0 +1,40 @@
+<div class="top-header">
+    <nav class="navbar has-shadow">
+        <div class="container is-fluid">
+            <div class="navbar-start">
+                <a href="{{ route('home') }}" class="navbar-item">
+                    <img src="https://bulma.io/images/bulma-logo.png" alt="DevClub.png" />
+                </a>
+                <a href="#" class="navbar-item is-tab is-hidden-mobile m-l-10">Learn</a>
+                <a href="#" class="navbar-item is-tab is-hidden-mobile">Discuess</a>
+                <a href="#" class="navbar-item is-tab is-hidden-mobile">Share</a>
+            </div>
+            <div class="navbar-end">
+                @if(Auth::guest())
+                    <a href="{{ route('login') }}" class="navbar-item is-tab">Login</a>
+                    <a href="{{ route('register') }}" class="navbar-item is-tab">Join The Community</a>
+                @else
+                    <button class="is-tab dropdown navbar-item">
+                        Hey Arnold <span class="icon"><i class="fa fa-caret-down"></i></span>
+
+                        <ul class="dropdown-menu">
+                            <li><a href="#"><span class="icon"><i class="fa fa-user-circle-o m-r-10"></i></span> Profile</a></li>
+                            <li><a href="#"><span class="icon"><i class="fa fa-bell m-r-10"></i></span>Notification</a></li>
+                            <li><a href="{{ route('manage.dashboard') }}"><span class="icon"><i class="fa fa-cog m-r-10"></i></span>Manage</a></li>
+                            <li class="seperator"></li>
+                            <li>
+                            <a href="{{ route('logout') }}" onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();
+                            ">
+                                <span class="icon">
+                                    <i class="fa fa-sign-out m-r-10"></i>
+                                </span>Logout</a>
+                                @include('_includes.forms.logout')
+                            </li>
+                        </ul>
+                    </button>
+                @endif
+            </div>
+        </div>
+    </nav> 
+</div>
--- resources/views/layouts/manage.blade.php
+++ resources/views/layouts/manage.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>DevClub - MANAGEMENT</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -16,6 +16,8 @@
 </head>
 <body >
     @include('_includes.nav.main')
+
+    @include('_includes.nav.manage')
     <div id="app">
         @yield('content')
     </div>
